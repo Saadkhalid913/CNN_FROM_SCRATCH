b252074f8fe89c8fe4a047e7453d3dc84a2efa46
Saad Khalid <saadkhalid913@gmail.com>
2021-12-05 23:49:20 -0500

    completed first nn model using first principles

---
+++ Logs/day-1.md
@@ -0,0 +1,2 @@
+## First day of NN from scratch
+### Going to be working on learning NN intuition + working on lin alg 
--- week-one/MLP.py
+++ week-one/MLP.py
@@ -1,6 +1,27 @@
 import numpy as np
+from sklearn.datasets import make_blobs
 import os 
 import json 
 import pickle
 import math
 import itertools
+
+class NN():
+    def __init__(self, x, y):
+        self.W1 = np.random.rand((x.shape[1], 4))
+        self.W2 = np.random.rand((6, y.shape[1]))
+
+    def Sigmoid(self, x, derivative = False):
+        if derivative:
+            return self.Sigmoid(x) * (1 - self.Sigmoid(x))
+        return 1 / 1 + np.exp(-x)
+
+    def forward(self, x):
+        out = np.dot(x, self.W1)
+        out = self.Sigmoid(out)
+        out = np.dot(out, self.W2)
+
+def CtoF(x):
+    return (x * 1.8) + 32
+
+print(np.random.randint((1, 150)))
